72fcbf41a77a1f62f4cd2dee6fb24a18fde871a4
Simon Wallace <simonwallace@users.noreply.github.com>
2020-03-27 20:14:08 +0000

    Added GetFileExtension extension method for ImageFormat

---
+++ WallpaperGenerator.Tests.Unit/ExtensionsTests/ImageFormatExtensionsTests/GetFileExtensionsTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Drawing.Imaging;
+using WallpaperGenerator.Extensions;
+using Xunit;
+
+namespace WallpaperGenerator.Tests.Unit.ExtensionsTests.ImageFormatExtensionsTests
+{
+    public class GetFileExtensionsTests
+    {
+        public static TheoryData<ImageFormat, string> TestData => new TheoryData<ImageFormat, string>
+        {
+            { ImageFormat.Bmp, "bmp" },
+            { ImageFormat.Emf, "emf" },
+            { ImageFormat.Exif, "exif" },
+            { ImageFormat.Gif, "gif" },
+            { ImageFormat.Icon, "ico" },
+            { ImageFormat.Jpeg, "jpg" },
+            { ImageFormat.MemoryBmp, "memorybmp" },
+            { ImageFormat.Png, "png" },
+            { ImageFormat.Tiff, "tif" },
+            { ImageFormat.Wmf, "wmf" }
+        };
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void When_I_supply_a_valid_image_format_Then_the_correct_file_extension_is_returned(ImageFormat format, string expected)
+        {
+            var actual = format.GetFileExtension();
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ WallpaperGenerator/Extensions/ImageFormatExtensions.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Drawing.Imaging;
+using System.Linq;
+
+namespace WallpaperGenerator.Extensions
+{
+    /// <summary>
+    /// Extensions to the <see cref="ImageFormat"/> class.
+    /// </summary>
+    public static class ImageFormatExtensions
+    {
+        /// <summary>
+        /// Returns the file extension associated with the image format.
+        /// </summary>
+        /// <param name="format">The image format.</param>
+        /// <returns>The file extension.</returns>
+        public static string GetFileExtension(this ImageFormat format)
+        {
+            var imageDecoder = ImageCodecInfo
+                .GetImageDecoders()
+                .FirstOrDefault(f => f.FormatID == format.Guid);
+
+            if (imageDecoder == null)
+            {
+                return format.GetDefaultFileExtension();
+            }
+
+            var fileExtension = imageDecoder
+                .FilenameExtension
+                .Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries)
+                .FirstOrDefault();
+
+            if (fileExtension == null)
+            {
+                return format.GetDefaultFileExtension();
+            }
+
+            fileExtension = fileExtension
+                .Trim('*', '.')
+                .ToLower();
+
+            return fileExtension;
+        }
+
+        /// <summary>
+        /// Returns the default file extension for the image format.
+        /// </summary>
+        /// <param name="format">The image format.</param>
+        /// <returns>The file extension.</returns>
+        private static string GetDefaultFileExtension(this ImageFormat format)
+        {
+            return format.ToString().ToLower();
+        }
+    }
+}
