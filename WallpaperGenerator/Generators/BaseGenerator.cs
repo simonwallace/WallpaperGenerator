98417ce8b33479e3fbdd436abc80474275c09879
Simon Wallace <simonwallace@users.noreply.github.com>
2020-03-31 19:30:08 +0100

    Added wallpaper generator base

---
+++ WallpaperGenerator/Generators/BaseGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+using WallpaperGenerator.Models;
+
+namespace WallpaperGenerator.Generators
+{
+    public abstract class BaseGenerator : IGenerator
+    {
+        public Bitmap Generate(Wallpaper config)
+        {
+            var bitmap = new Bitmap(config.Size.Width, config.Size.Height);
+
+            using (var graphics = Graphics.FromImage(bitmap))
+            {
+                graphics.Clear(config.BackgroundColour);
+
+                Draw(graphics, config);
+            }
+
+            return bitmap;
+        }
+
+        public abstract void Draw(Graphics graphics, Wallpaper config);
+    }
+}
---
+++ WallpaperGenerator/Generators/IGenerator.cs
@@ -0,0 +1,10 @@
+﻿using System.Drawing;
+using WallpaperGenerator.Models;
+
+namespace WallpaperGenerator.Generators
+{
+    public interface IGenerator
+    {
+        Bitmap Generate(Wallpaper config);
+    }
+}
